78ec85fe70f0a34c157f57a8f03fca3e5e42a53e
Laila Campbell <lailacampbell96@gmail.com>
2020-02-28 00:21:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,25 +13,3 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-end
-
-lass Book
-  attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
- 
-  GENRES = []
- 
-  def initialize(title)
-    @title = title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
- 
-  # create the writer for genre and add the logic for the class constant
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre 
-  end
-end
